4f1fd4745e99ecbf86003a960c1e35a65ce60904
Isaac McGaha Miller <isaacmcgahamiller@gmail.com>
2016-10-11 15:26:51 -0600

    part 1 done

--- js/app.js
+++ js/app.js
@@ -3,4 +3,22 @@
 
   $('select').material_select();
 
+  const app = angular.module('cameraApp', []);
+
+  app.filter('ratingStars', function () {
+  return function (input) {
+    let total = input;
+
+    let stars = '';
+
+    for (var i = 0; i < total; i++) {
+      stars += '*';
+    }
+
+    input = stars;
+
+    return input;
+
+  };
+});
 })();
